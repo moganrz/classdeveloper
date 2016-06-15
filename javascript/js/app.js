2ab0d4fb392721e6986f79f841acdba9132c5de3
Mogan <moganrz@gmail.com>
2016-06-15 20:11:25 -0300

    JavaScript 2

--- javascript/js/app.js
+++ javascript/js/app.js
@@ -2,3 +2,17 @@ function perguntaNome(){
   var nome = prompt("Qual e o seu nome?");
   alert("Ola " + nome);
 }
+
+function compararNumero(){
+  var numero = prompt("Digite um numero para ser comparado com 10");
+
+  if (numero < 10){
+    alert("Menor que 10");
+  }
+  else if (numero == 10) {
+      alert("Igual a 10");
+  }
+  else {
+      alert("Maior que 10");
+  }
+}
